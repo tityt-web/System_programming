518ec939202527fb2d4e0674df8e8677d9687229
tityt-web <ivankorykhov@yandex.ru>
2026-09-17 11:41:36 +0000

    Lab 1: fix 32-bit builds, disassembly, Work files and permissions

--- Lab_1/sandbox/korykhov.asm
+++ Lab_1/sandbox/korykhov.asm
@@ -1,17 +1,18 @@
-format ELF64 executable 3
+format ELF executable 3
+entry _start
+
+segment readable executable
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, msg_len
+    int 0x80
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
 
 segment readable writeable
 msg db 'Korykhov', 10, 'Ivan', 10, 'Andreevich', 10
 msg_len = $ - msg
-
-segment readable executable
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, msg_len
-    syscall
-
-    mov rax, 60
-    xor rdi, rdi
-    syscall
